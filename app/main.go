4ce989bbc3343691538ffa85c67c0a149cb743ee
mick-roper <mick.roper@dazn.com>
2019-07-29 17:33:16 +0100

    updated the wireup code

--- app/main.go
+++ app/main.go
@@ -9,11 +9,12 @@ import (
 )
 
 var port = flag.Int("port", 8080, "the port the server will listen on")
+var cpanelHostname = flag.String("cpanel-hostname", "", "the hostname of the cpanel")
 
 func main() {
 	flag.Parse()
 
 	addr := fmt.Sprintf(":%v", *port)
 
-	log.Fatal(proxy.Listen(addr))
+	log.Fatal(proxy.Listen(addr, *cpanelHostname))
 }
