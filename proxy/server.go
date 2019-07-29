c459a63f2d116b0975a69249e8e984edbd999212
mick-roper <mick.roper@dazn.com>
2019-07-29 17:31:28 +0100

    added the proxy handler

--- proxy/server.go
+++ proxy/server.go
@@ -2,8 +2,11 @@ package server
 
 import (
 	"errors"
+	"io"
 	"log"
 	"net/http"
+	"net/url"
+	"strings"
 	"time"
 
 	"github.com/gorilla/mux"
@@ -11,17 +14,27 @@ import (
 	handlers "./handlers"
 )
 
+var proxyData = make(map[string]string)
+var httpClient = &http.Client{}
+
 // Listen for HTTP traffic
-func Listen(addr string) error {
+func Listen(addr, cpanelHostname string) error {
 	if addr == "" {
 		return errors.New("addr is not defined")
 	}
 
+	if cpanelHostname == "" {
+		return errors.New("cpanelHostname is not defined")
+	}
+
 	r := mux.NewRouter()
 
-	r.PathPrefix("/api").Handler(handlers.NewAPIRequestRouter())
-	r.PathPrefix("/intercom").HandlerFunc(handlers.HandleSocketRequest)
-	r.HandleFunc("/", handlers.HandleHTMLRequest)
+	// CPANEL stuff
+	r.Host(cpanelHostname).PathPrefix("/api").PathPrefix("/api").Handler(handlers.NewAPIRequestRouter())
+	r.Host(cpanelHostname).PathPrefix("/intercom").HandlerFunc(handlers.HandleSocketRequest)
+	r.Host(cpanelHostname).Path("/").HandlerFunc(handlers.HandleHTMLRequest)
+
+	r.HandleFunc("/", proxyHandlerFunc)
 
 	server := &http.Server{
 		Handler:      r,
@@ -33,4 +46,61 @@ func Listen(addr string) error {
 	log.Println("SERVER: listening @", addr)
 
 	return server.ListenAndServe()
+}
+
+func proxyHandlerFunc(w http.ResponseWriter, r *http.Request) {
+	defer r.Body.Close()
+	proxyTarget := proxyData[r.Host]
+
+	if proxyTarget == "" {
+		w.WriteHeader(404)
+		w.Write([]byte("target not found for that hostname"))
+		return
+	}
+
+	u := url.URL{
+		Scheme: "http",
+		Host:   proxyTarget,
+		Path:   r.RequestURI,
+	}
+
+	pReq, err := http.NewRequest(r.Method, u.String(), r.Body)
+
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("an error occured while creating the proxy request"))
+		return
+	}
+
+	pResp, err := httpClient.Do(pReq)
+
+	if err != nil {
+		w.WriteHeader(500)
+		w.Write([]byte("an error occured while getting the proxy response"))
+		return
+	}
+
+	for key, values := range pResp.Header {
+		w.Header().Set(key, strings.Join(values, "; "))
+	}
+
+	w.WriteHeader(pResp.StatusCode)
+
+	if pResp.ContentLength != 0 {
+		buffer := make([]byte, 128*1024)
+
+		for {
+			i, err := pResp.Body.Read(buffer)
+
+			if err != nil {
+				if err == io.EOF {
+					w.Write(buffer[:i])
+				}
+
+				break
+			}
+
+			w.Write(buffer)
+		}
+	}
 }
